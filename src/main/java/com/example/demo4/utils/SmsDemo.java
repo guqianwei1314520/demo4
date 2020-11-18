c49014c7262b3eca4f76068c4fdccb71f3435978
guqianwei1314520 <38637858+guqianwei1314520@users.noreply.github.com>
2020-11-18 17:21:51 +0800

    Update SmsDemo.java

--- src/main/java/com/example/demo4/utils/SmsDemo.java
+++ src/main/java/com/example/demo4/utils/SmsDemo.java
@@ -32,8 +32,8 @@ public class SmsDemo {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAIPEYg03wW1nmu";
-    static final String accessKeySecret = "znrmHmYrseViSa9fRnB15sfpJu2yYs";
+    static final String accessKeyId = "";
+    static final String accessKeySecret = "";
 
     public static SendSmsResponse sendSms() throws ClientException {
 
